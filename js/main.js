dc6d4bf121e8ffcbd3ee0fc6643c3f236f8bdd29
Ilia-Seafarer <115416083+Ilia-Seafarer@users.noreply.github.com>
2022-12-12 22:04:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,107 @@
+const burger = document.querySelector(".burger");
+const container = document.querySelector(".container");
+const screens = document.querySelectorAll(".screen");
+
+burger.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+function replaceBackground(id) {
+  const backg = document.getElementById(id);
+  screens.forEach((screen) => {
+    screen.style.display = "none";
+  });
+  backg.style.display = "block";
+}
+
+function changeBackground() {
+  const links = document.querySelectorAll(".link");
+
+  links.forEach((link, index) => {
+    link.addEventListener("mouseenter", (e) => {
+      e.preventDefault();
+      replaceBackground(e.target.dataset.link);
+    });
+
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      container.classList.toggle("active");
+    });
+  });
+
+  screens.forEach((screen) => {
+    screen.style.display = "none";
+    screens[0].style.display = "block";
+  });
+}
+
+changeBackground();
+
+// telegram-contact-form
+
+$(".telegram-form").on("submit", function (event) {
+  event.stopPropagation();
+  event.preventDefault();
+
+  let form = this,
+    submit = $(".submit", form),
+    data = new FormData(),
+    files = $("input[type=file]");
+
+  $(".submit", form).val("Отправка...");
+  $("input, textarea", form).attr("disabled", "");
+
+  data.append("name", $('[name="name"]', form).val());
+  data.append("phone", $('[name="phone"]', form).val());
+  data.append("email", $('[name="email"]', form).val());
+  data.append("text", $('[name="text"]', form).val());
+  data.append("file", $('[name="file"]', form).val());
+
+  files.each(function (key, file) {
+    let cont = file.files;
+    if (cont) {
+      $.each(cont, function (key, value) {
+        data.append(key, value);
+      });
+    }
+  });
+
+  $.ajax({
+    url: "ajax.php",
+    type: "POST",
+    data: data,
+    cache: false,
+    dataType: "json",
+    processData: false,
+    contentType: false,
+    xhr: function () {
+      let myXhr = $.ajaxSettings.xhr();
+
+      if (myXhr.upload) {
+        myXhr.upload.addEventListener(
+          "progress",
+          function (e) {
+            if (e.lengthComputable) {
+              let percentage = (e.loaded / e.total) * 100;
+              percentage = percentage.toFixed(0);
+              $(".submit", form).html(percentage + "%");
+            }
+          },
+          false
+        );
+      }
+
+      return myXhr;
+    },
+    error: function (jqXHR, textStatus) {
+      // Тут выводим ошибку
+    },
+    complete: function () {
+      // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+      console.log("Complete");
+      form.reset();
+    },
+  });
+
+  return false;
+});
